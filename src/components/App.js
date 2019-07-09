b8b633622eb3c882c27a9c3743d4e905cd517c58
Grzegorz Szermiński <szerman@poczta.fm>
2019-07-09 22:11:04 +0200

    add deleteTask and changeTaskStatus functions

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import AddTask from "./AddTask";
 import TaskList from "./TaskList";
+import { format } from "url";
 
 class App extends React.Component {
   state = {
@@ -29,27 +30,50 @@ class App extends React.Component {
         important: false,
         active: true,
         finishDate: null
+      },
+      {
+        id: 15,
+        text: "Srutututu Sratitati bulubulu tramtatamtam",
+        date: "2020-08-01",
+        important: false,
+        active: true,
+        finishDate: null
       }
     ]
   };
 
   deleteTask = id => {
     console.log("delete elementu " + id);
-    const tasks = [...this.state.tasks];
-    console.log(tasks);
-
-    const index = tasks.findIndex(item => item.id === id);
-    tasks.splice(index, 1);
-    console.log(tasks);
+    // const tasks = [...this.state.tasks];
+    // const index = tasks.findIndex(item => item.id === id);
+    // tasks.splice(index, 1);
+    // this.setState({
+    //   tasks
+    // });
+    let tasks = [...this.state.tasks];
+    tasks = tasks.filter(item => item.id !== id);
+    this.setState({
+      tasks
+    });
   };
 
   changeTaskStatus = id => {
     console.log("change elementu " + id);
+    let tasks = Array.from(this.state.tasks);
+    tasks.forEach(item => {
+      if (item.id === id) {
+        item.active = false;
+        item.finishDate = new Date().getTime();
+      }
+    });
+    this.setState({
+      tasks
+    });
   };
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         ToDo lista
         <AddTask />
         <TaskList
